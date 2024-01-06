cd64e1d9520e3157b73a8d6048da025ea4f4a8a6
Guillaume Doré <guillaumedore.76@hotmail.fr>
2024-01-06 14:32:37 +0100

    add binance.Spot package

--- src/GoldenGui.CryptoBot.Functions/Function1.cs
+++ src/GoldenGui.CryptoBot.Functions/Function1.cs
@@ -1,25 +1,25 @@
+using Binance.Spot;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
 namespace GoldenGui.CryptoBot.Functions
 {
-	public class Function1
+	public class Function1(ILoggerFactory loggerFactory)
 	{
-		private readonly ILogger _logger;
-
-		public Function1(ILoggerFactory loggerFactory)
-		{
-			_logger = loggerFactory.CreateLogger<Function1>();
-		}
+		private readonly ILogger _logger = loggerFactory.CreateLogger<Function1>();
 
 		[Function("Function1")]
 		public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
 		{
-			_logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+			_logger.LogInformation("C# Timer trigger function executed at: {Now}", DateTime.Now);
+
+			Market market = new();
+			string serverTime = market.CheckServerTime().Result.ToString();
+			_logger.LogInformation("Server time: {ServerTime}", serverTime);
 
 			if (myTimer.ScheduleStatus is not null)
 			{
-				_logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+				_logger.LogInformation("Next timer schedule at: {Next}", myTimer.ScheduleStatus.Next);
 			}
 		}
 	}
